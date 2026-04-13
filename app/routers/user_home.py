df58f1a50da48f83b4b59e2013e5e76b9c0e73cc
BenPfister03 <benpfister03@gmail.com>
2026-04-12 21:42:49 -0400

    fix: pass workouts to home page template

--- app/routers/user_home.py
+++ app/routers/user_home.py
@@ -3,6 +3,8 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi import status
 from app.dependencies.session import SessionDep
 from app.dependencies.auth import AuthDep, IsUserLoggedIn, get_current_user, is_admin
+from app.models import Workout
+from sqlmodel import select
 from . import router, templates
 
 
@@ -10,12 +12,14 @@ from . import router, templates
 async def user_home_view(
     request: Request,
     user: AuthDep,
-    db:SessionDep
+    db: SessionDep
 ):
+    workouts = db.exec(select(Workout)).all()
     return templates.TemplateResponse(
         request=request, 
         name="app.html",
         context={
-            "user": user
+            "user": user,
+            "workouts": workouts
         }
     )
